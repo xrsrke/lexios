f0781c9c20a04a57d320904e9139ebf17472226f
xrsrke <xariusdrake@hotmail.com>
2022-11-20 10:06:01 +0700

    add find the shorest path

--- lexios/solver/graph.py
+++ lexios/solver/graph.py
@@ -40,6 +40,10 @@ class Graph:
         idx = [i for i, node in enumerate(self.find_edges(node1)) if node == node2]
         return self.weight[node1][idx[0]]
 
+    def find_weights(self, node: Node) -> List[List[Weight]]:
+        """List all the weights that associated with a node."""
+        return self.weight[node]
+
     def __repr__(self):
         """Representation."""
         return "\n".join([f"{node}: {neighbors}" for node, neighbors in enumerate(self.data)])
@@ -47,3 +51,86 @@ class Graph:
     def __str__(self):
         """String."""
         return self.__repr__()
+
+
+INFINITY = float("inf")
+
+
+class ShortestPath:
+    """Implementation of the shortest path algorithm."""
+
+    def __init__(self, graph: Graph):
+        """Initialize the shortest path algorithm."""
+        self.graph = graph
+        self.visited = [False] * len(graph.data)
+        self.parent = [None] * len(graph.data)
+        self.distance = [INFINITY] * len(graph.data)
+        self.queue = []
+
+    def find(self, source: Node, target: Node) -> Tuple[int, List[int]]:
+        """Find the shortest path from source to target."""
+        self.distance[source] = 0
+        self.distance[source] = 0
+        self.queue.append(source)
+
+        idx = 0
+
+        while idx < len(self.queue) and not self.visited[target]:
+            current = self.queue[idx]
+            self.visited[current] = True
+            idx += 1
+
+            # update the distance of all the neighbors
+            self.update_distance(current)
+
+            # find the unvisited node with the smallest distance
+            next_node = self.pick_next_node()
+            if next_node:
+                self.queue.append(next_node)
+
+        return self.distance[target], self.parent
+
+    def update_distance(self, current: Node):
+        """Update the distance of the current node to all its neighbors."""
+        neighbors = self.graph.find_edges(current)
+        weights = self.graph.find_weights(current)
+
+        for i, node in enumerate(neighbors):
+            weight = weights[i]
+
+            if self.distance[current] + weight < self.distance[node]:
+                self.distance[node] = self.distance[current] + weight
+                if self.parent:
+                    self.parent[node] = current
+
+    def pick_next_node(self) -> Node:
+        """Pick the next unvisited node at the smallest distance."""
+        min_distance = float("inf")
+        min_node = None
+
+        for node in range(len(self.distance)):
+            if not self.visited[node] and self.distance[node] < min_distance:
+                min_node = node
+                min_distance = self.distance[node]
+
+        return min_node
+
+
+def find_path_from_source_to_target(path: List[Node]) -> List[Node]:
+    """Find the path from the source to the target given the path from ShortestPath class."""
+    path_dict = {}
+
+    for i, node in enumerate(path):
+        path_dict[i] = node
+
+    target = list(path_dict.keys())[-1]
+    parent = target
+
+    target_to_source = [target]
+
+    while path_dict[parent]:
+        parent = path_dict[parent]
+        target_to_source.append(parent)
+
+    source_to_target = list(reversed(target_to_source))
+    return source_to_target
--- tests/solver/test_graph.py
+++ tests/solver/test_graph.py
@@ -1,6 +1,6 @@
 import pytest
 
-from lexios.solver.graph import Graph
+from lexios.solver.graph import Graph, ShortestPath, find_path_from_source_to_target
 
 
 @pytest.fixture
@@ -39,3 +39,37 @@ def test_graph_with_weights(graph_with_weights):
 def test_find_edges(graph_with_weights):
     assert graph_with_weights.find_edges(3) == [1, 2, 4]
     assert graph_with_weights.find_weight(1, 2) == 6
+
+
+@pytest.fixture
+def graph_with_weight2():
+    n_nodes = 9
+    edges = [
+        (0, 1, 3),
+        (0, 3, 2),
+        (0, 8, 4),
+        (1, 7, 4),
+        (2, 7, 2),
+        (2, 3, 6),
+        (2, 5, 1),
+        (3, 4, 1),
+        (4, 8, 8),
+        (5, 6, 8),
+    ]
+    return Graph(n_nodes, edges, weighted=True)
+
+
+def test_find_shortest_path(graph_with_weight2):
+
+    shorest_path = ShortestPath(graph_with_weight2)
+    length, path = shorest_path.find(2, 8)
+    assert length == 15
+
+
+def test_find_path_from_source_to_target(graph_with_weight2):
+    shorest_path = ShortestPath(graph_with_weight2)
+    _, path = shorest_path.find(2, 8)
+
+    path_from_source_to_target = find_path_from_source_to_target(path)
+
+    assert path_from_source_to_target == [2, 3, 4, 8]
